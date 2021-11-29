dfe7721102db3f9364eec5b2472e13fa41d258c2
youngjun00 <dudwns3421@naver.com>
2021-11-29 22:54:52 +0900

    변경(지금 결과 출력 부분 수정중)

--- coding.py
+++ coding.py
@@ -1,21 +1,9 @@
 from tkinter import *
-
-lapin = 0
-root = Tk()
-root.title("라플라스 변환 계산기")
-
-root.geometry("600x600")
-
-def btn1():
-    lapin=1
-    print(lapin)
-b1 = Button(root,width=10, height=6, text='1/s',command=btn1)
-b1.pack()
-
+from sympy import *
+from sympy.plotting.plot import MatplotlibBackend, _matplotlib_list
 import sqlite3 #sqlite 받아오기
 
 conn = sqlite3.connect("test.db") #test.db생성
-
 cur = conn.cursor() #커서생성
 
 cur.execute("CREATE TABLE IF NOT EXISTS customer (id integer PRIMARY KEY, lapnum text, lapdata text);") 
@@ -41,47 +29,161 @@ cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('9','exp(-a*t) * sin(w*
 cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('10','exp(-a*t) * cos(w*t)')")
 #lapnum 10 생성, data는 exp(-a*t) * cos(w*t)생성
 
-
-from sympy import *
-from sympy.plotting.plot import MatplotlibBackend, _matplotlib_list
-          
 x, y, z, t = symbols('x y z t')
 f, g, h = symbols('f, g, h', cls=Function)
 
 init_printing()
 
-# 라플라스 모듈 불러오기 
 from sympy.integrals.transforms import laplace_transform
 
 s = symbols('s')
 a, w = symbols('a w', constant=True, positive=True)
 
-if lapin == 1:
-    sql = "select lapdata from customer where lapnum = ?"
-    cur.execute(sql, ('1'))
-    rows = cur.fetchall()
-    for row in rows:
-        str = ''.join(row)
-        lapresult = laplace_transform(str, t, s,noconds=True)
-        print(lapresult)
+lapin=0
+root = Tk()
+root.title("라플라스 변환 계산기")
+sql = "select lapdata from customer where lapnum = ?"
 
-elif lapin == 2:
-    sql = "select lapdata from customer where lapnum = ?"
-    cur.execute(sql, ('2'))
-    rows = cur.fetchall()
-    for row in rows:
-        str = ''.join(row)
-        lapresult = laplace_transform(str, t, s,noconds=True)
-        print(lapresult)
-        
-elif lapin == 3:
-    sql = "select lapdata from customer where lapnum = ?"
-    cur.execute(sql, ('3'))
-    rows = cur.fetchall()
-    for row in rows:
-        str = ''.join(row)
-        lapresult = laplace_transform(str, t, s,noconds=True)
-        print(lapresult)
-        
+root.geometry("600x600")
+def btn1():
+    lapin=1
+    print(lapin)
+    
+def btn2():
+    lapin=2
+    print(lapin)
+    
+def btn3():
+    lapin=3
+    print(lapin)
+    
+def btn4():
+    lapin=4
+    print(lapin)
+    
+def btn5():
+    lapin=5
+    print(lapin)
+    
+def btn6():
+    lapin=6
+    print(lapin)
+    
+def btn7():
+    lapin=7
+    print(lapin)
+    
+def btn8():
+    lapin=8
+    print(lapin)
+    
+def btn9():
+    lapin=9
+    print(lapin)
+    
+def btn10():
+    lapin=10
+    print(lapin)
+    
+def btn11():
+    if lapin == 1:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('1'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 2:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('2'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 3:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('3'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 4:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('4'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True) 
+    elif lapin == 5:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('5'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)  
+    elif lapin == 6:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('6'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 7:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('7'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)  
+    elif lapin == 8:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('8'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 9:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('9'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True) 
+    elif lapin == 10:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('10'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    labelresulte.config(text = lapresult)
+    print(lapresult)
+    
+b1 = Button(root,width=6, height=3, text='1/s',command=btn1)
+b1.grid(row=0,column=0)
+b2 = Button(root,width=6, height=3, text='1/s',command=btn2)
+b2.grid(row=0,column=1)
+b3 = Button(root,width=6, height=3, text='1/s',command=btn3)
+b3.grid(row=0,column=2)
+b4 = Button(root,width=6, height=3, text='1/s',command=btn4)
+b4.grid(row=1,column=0)
+b5 = Button(root,width=6, height=3, text='1/s',command=btn5)
+b5.grid(row=1,column=1)
+b6 = Button(root,width=6, height=3, text='1/s',command=btn6)
+b6.grid(row=1,column=2)
+b7 = Button(root,width=6, height=3, text='1/s',command=btn7)
+b7.grid(row=2,column=0)
+b8 = Button(root,width=6, height=3, text='1/s',command=btn8)
+b8.grid(row=2,column=1)
+b9 = Button(root,width=6, height=3, text='1/s',command=btn9)
+b9.grid(row=2,column=2)
+b10 = Button(root,width=6, height=3, text='1/s',command=btn10)
+b10.grid(row=3,column=0)
+b11 = Button(root,width=6, height=3, text='변환',command=btn11)
+b11.grid(row=3,column=1)
+
+labelresulte = Label(root, text = "답")
+labelresulte.grid(row=3,column=2)
+
 root.mainloop
 conn.close
