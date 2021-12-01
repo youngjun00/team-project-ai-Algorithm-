5ebeb8705c76b95d689ddfb3724c4680df44359e
youngjun00 <89117576+youngjun00@users.noreply.github.com>
2021-12-01 13:25:10 +0900

    Update 20211117.py

--- 20211117.py
+++ 20211117.py
@@ -1,100 +1,189 @@
-#include <math.h>
-#include <stdio.h>
-
-
+from tkinter import *
 from sympy import *
 from sympy.plotting.plot import MatplotlibBackend, _matplotlib_list
-          
+import sqlite3 #sqlite 받아오기
+
+conn = sqlite3.connect("test.db") #test.db생성
+cur = conn.cursor() #커서생성
+
+cur.execute("CREATE TABLE IF NOT EXISTS customer (id integer PRIMARY KEY, lapnum text, lapdata text);") 
+#테이블 생성(primary key 사용 lapnum은 번호, lapdata는 라플라스 변수)
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('1','DiracDelta(t)')")
+#lapnum 1 생성, data는 diracdelta(t)생성
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('2','Heaviside(t)')")
+#lapnum 2 생성, data는 Heaviside(t)생성
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('3','exp(-a*t)')")
+#lapnum 3 생성, data는 t*exp(-a*t)생성
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('4','sin(w*t)')")
+#lapnum 4 생성, data는 sin(w*t)생성
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('5','sinh(w*t)')")
+#lapnum 5 생성, data는 sinh(w*t)생성
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('6','cosh(w*t)')")
+#lapnum 6 생성, data는 cosh(w*t)생성
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('7','t * sin(w*t)')")
+#lapnum 7 생성, data는 t * sin(w*t)생성
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('8','t * cos(w*t)')")
+#lapnum 8 생성, data는 t * cos(w*t)생성
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('9','exp(-a*t) * sin(w*t)')")
+#lapnum 9 생성, data는 exp(-a*t) * sin(w*t)생성
+cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('10','exp(-a*t) * cos(w*t)')")
+#lapnum 10 생성, data는 exp(-a*t) * cos(w*t)생성
+
 x, y, z, t = symbols('x y z t')
 f, g, h = symbols('f, g, h', cls=Function)
 
 init_printing()
 
-
-# 라플라스 모듈 불러오기 
 from sympy.integrals.transforms import laplace_transform
-from sympy.integrals.transforms import inverse_laplace_transform
 
 s = symbols('s')
 a, w = symbols('a w', constant=True, positive=True)
-lap1 =1 
-if lap1 == 2:
-   laplace_transform(Heaviside(t), t, s, noconds=True)##Heaviside(t)=u(t)  2번
-elif lap1 == 3:
-   laplace_transform( t*exp(-a*t), t, s, noconds=True)## 7번 
-elif lap1 == 4:
-   laplace_transform(sin(w*t), t, s, noconds=True )## 8번
-elif lap1 == 5:
-   laplace_transform(cos(w*t), t, s, noconds=True )## 9번
-elif lap1 == 6:
-   laplace_transform( sinh(w*t), t, s, noconds=True)## 10번
-#elif lap1 == 7:
-#laplace_transform( cosh(w*t), t, s, noconds=True)## 11번 해결안됨.
-elif lap1 == 8:
-   laplace_transform(t * sin(w*t), t, s, noconds=True )## 12번
-elif lap1 == 9:
-   laplace_transform(t * cos(w*t), t, s, noconds=True )## 13번
-elif lap1 == 10:
-   laplace_transform(exp(-a*t) * sin(w*t), t, s, noconds=True) ## 14번
-elif lap1 == 11:
-   laplace_transform(exp(-a*t) * cos(w*t), t, s, noconds=True )## 15번
 
-
-
-import sqlite3
- 
-# DB 생성 (오토 커밋)
-conn = sqlite3.connect("test.db")
-
-cur = conn.cursor()
-
-cur.execute("CREATE TABLE IF NOT EXITS customer (idinteger PRIMARY KEY , lapnum text, lapdata text);")
-
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('1','Heaviside(t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('2','t*exp(-a*t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('3','sin(w*t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('4','cos(w*t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('5','sinh(w*t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('6','t * sin(w*t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('7','t * cos(w*t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('8','exp(-a*t) * sin(w*t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('9','(exp(-a*t) * cos(w*t)')")
-
-sp1 = "select lapdata from customer where lapnum = ?"
-
-if num = 1:
-   cur.execute(sp1,('1'))
-   rows = cur.fetchall()
-
-for row in rows:
-   print(row)
-
-conn.close
-
-laplace_transform( DiracDelta(t), t, s,noconds=True)
-
-import sqlite3
-
-conn = sqlite3.connect("test.db")
-
-cur = conn.cursor()
-
-cur.execute("CREATE TABLE IF NOT EXISTS customer (id integer PRIMARY KEY, lapnum text, lapdata text);")
-
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('1','DiracDelta(t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('2','Heaviside(t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('1','DiracDelta(t)')")
-cur.execute("INSERT INTO customer(lapnum,lapdata) VALUES('2','Heaviside(t)')")
-
+lapin=0
+root = Tk()
+root.title("라플라스 변환 계산기")
 sql = "select lapdata from customer where lapnum = ?"
 
-cur.execute(sql, ('1'))
-rows = cur.fetchall()
+root.geometry("600x600")
+def btn1():
+    lapin=1
+    print(lapin)
+    
+def btn2():
+    lapin=2
+    print(lapin)
+    
+def btn3():
+    lapin=3
+    print(lapin)
+    
+def btn4():
+    lapin=4
+    print(lapin)
+    
+def btn5():
+    lapin=5
+    print(lapin)
+    
+def btn6():
+    lapin=6
+    print(lapin)
+    
+def btn7():
+    lapin=7
+    print(lapin)
+    
+def btn8():
+    lapin=8
+    print(lapin)
+    
+def btn9():
+    lapin=9
+    print(lapin)
+    
+def btn10():
+    lapin=10
+    print(lapin)
+    
+def btn11():
+    if lapin == 1:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('1'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 2:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('2'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 3:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('3'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 4:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('4'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True) 
+    elif lapin == 5:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('5'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)  
+    elif lapin == 6:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('6'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 7:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('7'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)  
+    elif lapin == 8:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('8'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    elif lapin == 9:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('9'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True) 
+    elif lapin == 10:
+        sql = "select lapdata from customer where lapnum = ?"
+        cur.execute(sql, ('10'))
+        rows = cur.fetchall()
+        for row in rows:
+            str1 = ''.join(row)
+            lapresult = laplace_transform(str1, t, s,noconds=True)
+    labelresulte.config(text = lapresult)
+    print(lapresult)
+    
+b1 = Button(root,width=6, height=3, text='1/s',command=btn1)
+b1.grid(row=0,column=0)
+b2 = Button(root,width=6, height=3, text='1/s',command=btn2)
+b2.grid(row=0,column=1)
+b3 = Button(root,width=6, height=3, text='1/s',command=btn3)
+b3.grid(row=0,column=2)
+b4 = Button(root,width=6, height=3, text='1/s',command=btn4)
+b4.grid(row=1,column=0)
+b5 = Button(root,width=6, height=3, text='1/s',command=btn5)
+b5.grid(row=1,column=1)
+b6 = Button(root,width=6, height=3, text='1/s',command=btn6)
+b6.grid(row=1,column=2)
+b7 = Button(root,width=6, height=3, text='1/s',command=btn7)
+b7.grid(row=2,column=0)
+b8 = Button(root,width=6, height=3, text='1/s',command=btn8)
+b8.grid(row=2,column=1)
+b9 = Button(root,width=6, height=3, text='1/s',command=btn9)
+b9.grid(row=2,column=2)
+b10 = Button(root,width=6, height=3, text='1/s',command=btn10)
+b10.grid(row=3,column=0)
+b11 = Button(root,width=6, height=3, text='변환',command=btn11)
+b11.grid(row=3,column=1)
 
+labelresulte = Label(root, text = "답")
+labelresulte.grid(row=3,column=2)
 
-for row in rows:
-    str = ''.join(row)
-    print(str)
-
-laplace_transform(str, t, s,noconds=True)
+root.mainloop
 conn.close
